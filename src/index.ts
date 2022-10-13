16f31c343850ec12f7547446411ce405a0ec4a15
masha14013 <masha14013@rambler.ru>
2022-10-13 16:54:22 +0300

    added app Hello World

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, {Request, Response} from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+    let helloMessage = 'Hello World!!! Hello world&&&12'
+    res.send(helloMessage)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
